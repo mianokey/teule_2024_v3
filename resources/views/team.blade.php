6da41606eb36bdb5b0e2de2ba8f3eaa1f9db62ad
mianokey <mianokey@gmail.com>
2026-01-20 14:42:44 +0300

    sorted add register user

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -29,7 +29,7 @@
           <div class="col-sm-6 col-lg-3">
             <div class="team-item">
               <div class="top" >
-                <img src="{{ asset($member['img_url']) }}"  alt="{{$member['name']}} image" />
+                <img src="{{ asset($member['img_url'] ?? 'uploads/avatar.png') }}" alt="{{$member['name']}} image" />
               </div>
               <div class="bottom">
                 <h3>{{$member['name']}}</h3>
